e7b8c9399ccebe44cc8c7a157644f74faa35174c
Vladislav Kuznecov <fizvlad@mail.ru>
2021-01-13 16:50:28 +0300

    feat: devise migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/.freeze
 
   validates :email, format: EMAIL_REGEX
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
