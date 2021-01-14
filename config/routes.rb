afec81133992fe1d848633cd9816ff178443f750
Vladislav Kuznecov <fizvlad@mail.ru>
2021-01-14 14:30:35 +0300

    feat: restrict registration when admin exists

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin/rails', as: 'rails_admin'
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   devise_scope :user do
-    get 'signin', to: 'devise/sessions#new', as: :signin
-    post 'signin', to: 'devise/sessions#create'
-    delete 'signout', to: 'devise/sessions#destroy', as: :signout
-    get 'signup', to: 'devise/registrations#new', as: :signup
+    get 'signin', to: 'users/sessions#new', as: :signin
+    post 'signin', to: 'users/sessions#create'
+    delete 'signout', to: 'users/sessions#destroy', as: :signout
+    get 'signup', to: 'users/registrations#new', as: :signup
   end
 
   root to: 'pages#root'
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -5,9 +5,18 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET devise/registrations#new' do
     it :aggregate_failures do
-      get '/users/sign_up'
+      get signup_path
       expect(response).to render_template('layouts/application')
       expect(response).to have_http_status(:ok)
+    end
+
+    context 'when admin user exists' do
+      before { User.create(email: 'example@mail.com', password: 'password', admin: true) }
+
+      it do
+        get signup_path
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
