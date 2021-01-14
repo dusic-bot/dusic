9a7ae297308b50ccf27335afedec6cf31310b13d
Vladislav Kuznecov <fizvlad@mail.ru>
2021-01-14 12:17:33 +0300

    feat: install

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,25 @@
+RailsAdmin.config do |config|
+  ### Popular gems integration
+
+  ## == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
+
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin/rails', as: 'rails_admin'
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
   root to: 'pages#root'
