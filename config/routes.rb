b3ee5f5d596e51377af09e4893353f2b6df5562b
Vladislav Kuznecov <fizvlad@mail.ru>
2021-01-14 13:32:53 +0300

    feat: devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin/rails', as: 'rails_admin'
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new', as: :signin
+    post 'signin', to: 'devise/sessions#create'
+    delete 'signout', to: 'devise/sessions#destroy', as: :signout
+    get 'signup', to: 'devise/registrations#new', as: :signup
+  end
 
   root to: 'pages#root'
 end
