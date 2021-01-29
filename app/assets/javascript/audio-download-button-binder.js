c09bba8065af05ab4ae3bb2a60a08da0a09be2ee
Vladislav Kuznecov <fizvlad@mail.ru>
2021-01-29 22:24:51 +0300

    feat: stub audio downloading

--- app/assets/javascript/audio-download-button-binder.js
+++ app/assets/javascript/audio-download-button-binder.js
@@ -1,19 +1,32 @@
 import jQuery from 'jquery';
+import { sleep } from 'javascript/sleep';
+
+function audioDownload(manager, id, format) {
+  window.console.debug(`Loading ${format}: ${manager}#${id}`);
+  // TODO
+  return sleep(5000);
+}
 
 function audioButtonClick() {
   const $button = jQuery(this);
+  if ($button.prop('disabled')) return;
+
   const { manager, id } = $button.data();
+  const format = 'm3u8'; // TODO: set via interface
 
-  window.console.debug(`${manager}: ${id}`);
-
-  // const oldHtml = $button.html();
+  const oldHtml = $button.html();
   $button.html('<i class="fa fa-spinner" aria-hidden="true"></i>');
   $button.prop('disabled', true);
   $button.children().first().addClass('spinning');
+
+  audioDownload(manager, id, format).then(() => {
+    $button.html(oldHtml);
+    $button.prop('disabled', false);
+  });
 }
 
 jQuery(document).ready(() => {
-  window.console.debug('audio-download-button-binder');
+  window.console.debug('Fired audio-download-button-binder');
 
   const buttons = jQuery('.js-audio-download-button-binder');
 
