c26a751aa0256227a2f2607a1e497e084280a2c9
Vladislav Kuznecov <fizvlad@mail.ru>
2021-02-02 23:55:17 +0300

    feat: Date.current -> Time.zone.today

--- factories/models/daily_statistic.rb
+++ factories/models/daily_statistic.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :daily_statistic do
     discord_server
-    date { Date.current }
+    date { Time.zone.today }
     tracks_length { 0 }
     tracks_amount { 0 }
   end
