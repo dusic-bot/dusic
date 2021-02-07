a317321ca8b63ee245e0663334e496acfb22236a
Vladislav Kuznecov <fizvlad@mail.ru>
2021-02-07 15:08:19 +0300

    wip: audio respnse items rendering

---
+++ app/blueprints/audio_blueprint.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AudioBlueprint < Blueprinter::Base
+  field :id
+  field :artist
+  field :title
+  field :duration
+end
---
+++ app/blueprints/playlist_blueprint.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class PlaylistBlueprint < Blueprinter::Base
+  field :id
+end
