4c2429bd5d74ddfb4e4f9fe1c86e54da9779c9f9
Vladislav Kuznecov <fizvlad@mail.ru>
2021-02-26 21:16:17 +0300

    feat: actions broadcaster

---
+++ app/services/shard_action_broadcaster_service.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ShardActionBroadcasterService
+  def self.call(shard, action)
+    ActionCable.server.broadcast(
+      "shards/#{shard.identifier}",
+      action: action
+    )
+  end
+end
