82726271e2917ce7a5ea660207eca4812ff53d2a
Vladislav Kuznecov <fizvlad@mail.ru>
2021-03-21 15:49:57 +0300

    feat: connection data ws action

--- app/channels/api/v2/shards_channel.rb
+++ app/channels/api/v2/shards_channel.rb
@@ -7,6 +7,12 @@ module Api
         stream_from 'shards'
         stream_from "shards/#{current_shard.identifier}"
       end
+
+      def connection_data(raw_data)
+        current_shard.servers_count = raw_data['servers_count'].to_i
+        current_shard.cached_servers_count = raw_data['cached_servers_count'].to_i
+        current_shard.active_servers_count = raw_data['active_servers_count'].to_i
+      end
     end
   end
 end
--- spec/channels/api/v2/shards_channel_spec.rb
+++ spec/channels/api/v2/shards_channel_spec.rb
@@ -7,10 +7,37 @@ RSpec.describe Api::V2::ShardsChannel, type: :channel do
 
   before { stub_connection(current_shard: current_shard) }
 
-  it :aggregate_failures do
-    subscribe
-    expect(subscription).to be_confirmed
-    expect(subscription).to have_stream_from('shards')
-    expect(subscription).to have_stream_from('shards/0_1_42')
+  describe '#subscribed' do
+    it :aggregate_failures do
+      subscribe
+      expect(subscription).to be_confirmed
+      expect(subscription).to have_stream_from('shards')
+      expect(subscription).to have_stream_from('shards/0_1_42')
+    end
+  end
+
+  describe '#connection_data' do
+    let(:data) { {} }
+
+    before do
+      subscribe
+      perform :connection_data, **data
+    end
+
+    it :aggregate_failures do
+      expect(current_shard.servers_count).to eq(0)
+      expect(current_shard.cached_servers_count).to eq(0)
+      expect(current_shard.active_servers_count).to eq(0)
+    end
+
+    context 'when data is correct' do
+      let(:data) { { 'servers_count' => 2, 'cached_servers_count' => 1, 'active_servers_count' => 0 } }
+
+      it :aggregate_failures do
+        expect(current_shard.servers_count).to eq(2)
+        expect(current_shard.cached_servers_count).to eq(1)
+        expect(current_shard.active_servers_count).to eq(0)
+      end
+    end
   end
 end
