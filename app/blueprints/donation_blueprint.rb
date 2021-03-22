62aeb5891761958bfb220fa6ccae924f02a2ab6b
Vladislav Kuznecov <fizvlad@mail.ru>
2021-03-22 23:12:52 +0300

    refactor: remove discord and external keywords (#19)

--- app/blueprints/donation_blueprint.rb
+++ app/blueprints/donation_blueprint.rb
@@ -5,10 +5,10 @@ class DonationBlueprint < Blueprinter::Base
 
   field :size
   field :date
-  field :discord_user_external_id do |donation|
+  field :user_id do |donation|
     donation.discord_user&.external_id
   end
-  field :discord_server_external_id do |donation|
+  field :server_id do |donation|
     donation.discord_server&.external_id
   end
 end
