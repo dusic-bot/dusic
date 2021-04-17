52662c616117ea2f97c1dc894dfe0d6e1502c3e9
Vladislav Kuznecov <fizvlad@mail.ru>
2021-04-17 13:24:47 +0300

    feat: shorten donation check interval

--- app/jobs/vkponchik_donations_check_job.rb
+++ app/jobs/vkponchik_donations_check_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class VkponchikDonationsCheckJob < ApplicationJob
-  INTERVAL = 45.minutes # TODO: AFTER FULL RELEASE: minutes -> seconds
+  INTERVAL = 45.seconds
 
   queue_as :donations
 
