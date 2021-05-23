0a5fe890ea7d9457670a7d41be7b340207ff6207
Vladislav Kuznecov <fizvlad@mail.ru>
2021-05-23 12:56:00 +0300

    feat: check in jwt authorizer

--- spec/services/jwt_authorizer_service_spec.rb
+++ spec/services/jwt_authorizer_service_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe JwtAuthorizerService do
-  subject(:result) { described_class.call(token, access_level: required_access_level) }
+  subject(:result) { described_class.call(token, controller: required_controller) }
 
   let(:token) { nil }
-  let(:required_access_level) { 42 }
+  let(:required_controller) { 'first' }
 
   it { expect(result).to be(false) }
 
@@ -14,30 +14,42 @@ RSpec.describe JwtAuthorizerService do
     let(:token) { '' }
 
     it { expect(result).to be(false) }
+  end
 
-    context 'when incorrect header present' do
-      let(:token) { 'Pizza time' }
+  context 'when incorrect header present' do
+    let(:token) { 'Pizza time' }
+
+    it { expect(result).to be(false) }
+  end
+
+  context 'when correct header present' do
+    let(:token) { JwtEncoderService.call(access: { controllers: allowed_controllers }) }
+    let(:allowed_controllers) { %w[first] }
+
+    it { expect(result).to be(true) }
+
+    context 'when JwtDecoderService raises error' do
+      before { allow(JwtDecoderService).to receive(:call).and_raise(StandardError) }
 
       it { expect(result).to be(false) }
     end
 
-    context 'when correct header present' do
-      let(:token) { JwtEncoderService.call(access_level: access_level) }
-      let(:access_level) { 100500 }
+    context 'when access level is incorrect' do
+      let(:allowed_controllers) { %w[second] }
+
+      it { expect(result).to be(false) }
+    end
+
+    context 'when several allowed controllers' do
+      let(:allowed_controllers) { %w[first second] }
 
       it { expect(result).to be(true) }
+    end
 
-      context 'when JwtDecoderService raises error' do
-        before { allow(JwtDecoderService).to receive(:call).and_raise(StandardError) }
+    context 'when several allowed controllers without required one' do
+      let(:allowed_controllers) { %w[second third] }
 
-        it { expect(result).to be(false) }
-      end
-
-      context 'when access level is low' do
-        let(:access_level) { 0 }
-
-        it { expect(result).to be(false) }
-      end
+      it { expect(result).to be(false) }
     end
   end
 end
