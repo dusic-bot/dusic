e62c499c6fa8829f87fda774c1cdd5dc90f0d0d6
Vladislav Kuznecov <fizvlad@mail.ru>
2021-08-04 00:28:20 +0300

    feat: switch locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
   private
+
+  def switch_locale(&action)
+    locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   def admin_check
     return if current_user&.admin
