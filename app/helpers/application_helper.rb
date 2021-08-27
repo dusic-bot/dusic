08ffd51202157c085bdd9afe1dbcdc2563bb8cfd
Vladislav Kuznecov <fizvlad@mail.ru>
2021-08-28 00:31:53 +0300

    feat: title_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
   DISCORD_SERVER_URL = 'https://discord.gg/4sUeCmQ'
   BOT_INVITE_URL = 'https://discord.com/oauth2/authorize?client_id=479945608824619009&scope=bot&permissions=3213312'
   SOURCE_CODE_URL = 'https://github.com/dusic-bot'
+
+  def title_tag(title, env_prefix: true)
+    return tag.title(title) unless env_prefix
+
+    prefix =
+      case Rails.env
+      when 'development' then 'DEV'
+      else ''
+      end
+    return tag.title(title) if prefix.blank?
+
+    tag.title("#{prefix} | #{title}")
+  end
 end
