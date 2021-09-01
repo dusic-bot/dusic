c6cc32e5f0076fd9b64f89da70c8fa5783e5ff21
Vladislav Kuznecov <fizvlad@mail.ru>
2021-09-01 21:45:51 +0300

    fix: doubled entry in linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,8 @@ set :deploy_to, '/home/dusic/dusic'
 append :linked_files, 'config/credentials/production.key'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', '.bundle',
-       'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads',
+       'vendor/bundle', '.bundle'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
