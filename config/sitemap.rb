3c9f1b4f7cb9706778e1e1b432dd7ad8b0dcb850
Vladislav Kuznecov <fizvlad@mail.ru>
2021-09-01 22:07:45 +0300

    feat: sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = 'https://dusic.fizvlad.com'
 
+# Do not compress XML yet
+SitemapGenerator::Sitemap.compress = false
+
 SitemapGenerator::Sitemap.create
