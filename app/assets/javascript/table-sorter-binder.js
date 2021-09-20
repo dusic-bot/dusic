e879e405ed18a8ead1207ac7e0b0a814c73b99dc
Vladislav Kuznecov <fizvlad@mail.ru>
2021-09-20 21:29:42 +0300

    feat: table sorter

--- app/assets/javascript/table-sorter-binder.js
+++ app/assets/javascript/table-sorter-binder.js
@@ -1,18 +1,72 @@
 import jQuery from 'jquery';
 
-function headCellClick(event) {
-  const index = event.data.headCells.indexOf(this);
+const HEAD_CELLS_CSS_SELECTOR = 'thead td:not([colspan])';
+
+function parseCellValue(cell) {
+  return cell.innerText;
+}
+
+function IsEqualHTMLCollection(collectionA, collectionB) {
+  if (collectionA.length !== collectionB.length) return false;
+
+  for (let index = 0; index < collectionA.length; index += 1) {
+    if (collectionA[index] !== collectionB[index]) return false;
+  }
+
+  return true;
+}
+
+function spaceshipCompare(a, b) {
+  if (a === b) {
+    return 0;
+  }
+
+  if (a < b) {
+    return -1;
+  }
+
+  return 1;
+}
+
+function sortLines(lines, index, invert = false) {
+  return lines.sort((lineA, lineB) => {
+    const cellA = jQuery(lineA).find('td').toArray()[index];
+    const cellB = jQuery(lineB).find('td').toArray()[index];
+
+    let cellValueA = parseCellValue(cellA);
+    let cellValueB = parseCellValue(cellB);
+
+    if (invert) {
+      [cellValueA, cellValueB] = [cellValueB, cellValueA];
+    }
+
+    return spaceshipCompare(cellValueA, cellValueB);
+  });
+}
+
+function headCellClick() {
+  const $table = jQuery(this).closest('table');
+  const $headCells = $table.find(HEAD_CELLS_CSS_SELECTOR);
+
+  const index = $headCells.toArray().indexOf(this);
 
   window.console.debug(`Sorting table by column#${index}`);
+  const lines = $table.find('tbody tr').toArray();
+  const sortedLines = sortLines([...lines], index);
 
-  // TODO
+  if (IsEqualHTMLCollection(sortedLines, lines)) {
+    sortedLines.reverse();
+  }
+
+  lines.forEach((line, lineIndex) => {
+    line.parentElement.appendChild(sortedLines[lineIndex]);
+  });
 }
 
 function bind(table) {
   const $table = jQuery(table);
 
-  const $headCells = $table.find('thead td:not([colspan])');
-  $headCells.on('click', { table, headCells: $headCells.toArray() }, headCellClick);
+  $table.find(HEAD_CELLS_CSS_SELECTOR).on('click', headCellClick);
 }
 
 jQuery(() => {
@@ -20,7 +74,5 @@ jQuery(() => {
 
   const tables = jQuery('.js-table-sorter-binder');
 
-  tables.each((_i, table) => {
-    bind(table);
-  });
+  tables.each((_i, table) => { bind(table); });
 });
