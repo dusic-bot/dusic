2a00903073bb0dff619464019c67159010a738d8
Vladislav Kuznecov <fizvlad@mail.ru>
2022-03-20 13:21:35 +0300

    fix: f-e

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,18 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
