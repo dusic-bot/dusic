d26fab23fd87818939263bece0d3d6b2c44eacfc
Vladislav Kuznecov <fizvlad@mail.ru>
2022-03-20 14:14:03 +0300

    feat: exclude test from allowed NODE_ENV

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
+  var validEnv = ['development', 'production']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
   var isProductionEnv = api.env('production')
