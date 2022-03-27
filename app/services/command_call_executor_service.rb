f3cdb54b9d90fa2177992eece319b932f6fa480e
Vladislav Kuznecov <fizvlad@mail.ru>
2022-03-27 17:02:02 +0300

    feat: command call broadcasting mvp

--- app/services/command_call_executor_service.rb
+++ app/services/command_call_executor_service.rb
@@ -4,14 +4,15 @@ class CommandCallExecutorService
   class << self
     def call(shard_identifier, payload)
       shard = find_shard(shard_identifier)
+      return if shard.nil?
 
-      # TODO
+      ShardActionBroadcasterService.call(shard, 'command_call', JSON.parse(payload))
     end
 
     private
 
     def find_shard(identifier)
-      ActionCable.server.connections.find { |s| s.current_shard.identifier == identifier }
+      ActionCable.server.connections.find { |s| s.current_shard.identifier == identifier }&.current_shard
     end
   end
 end
--- spec/services/command_call_executor_service_spec.rb
+++ spec/services/command_call_executor_service_spec.rb
@@ -3,7 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe CommandCallExecutorService do
-  subject(:result) { described_class.call(shard_identifier, payload) }
+  subject(:call) { described_class.call(shard_identifier, payload) }
 
-  xit { expect(result).to be_nil }
+  let(:shard_identifier) { shard.identifier }
+  let(:payload) { '{"stub": true}' }
+  let(:shard) { build(:shard_connection_data) }
+
+  let(:connection_stub) { instance_double('ApplicationCable::Connection', current_shard: shard) }
+
+  before do
+    allow(ActionCable.server.connections).to receive(:find).and_return(connection_stub)
+  end
+
+  it 'broadcasting message to shard', :aggregate_failures do
+    expect(ShardActionBroadcasterService).to receive(:call).with(shard, 'command_call', { 'stub' => true})
+    expect { call }.not_to raise_error
+  end
+
+  context 'when shard does not exist' do
+    let(:connection_stub) { nil }
+
+    it 'fails silently', :aggregate_failures do
+      expect(ShardActionBroadcasterService).not_to receive(:call)
+      expect { call }.not_to raise_error
+    end
+  end
 end
