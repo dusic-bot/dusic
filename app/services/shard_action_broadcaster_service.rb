a41fbdb04fde2052d9c14d52f7bea7fb0044c2a5
Vladislav Kuznecov <fizvlad@mail.ru>
2022-03-27 14:43:55 +0300

    feat: pass payload when broadcasting to shard

--- app/services/shard_action_broadcaster_service.rb
+++ app/services/shard_action_broadcaster_service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ShardActionBroadcasterService
-  def self.call(shard, action)
-    ActionCable.server.broadcast "shards/#{shard.identifier}", { action: }
+  def self.call(shard, action, payload = nil)
+    ActionCable.server.broadcast "shards/#{shard.identifier}", { action:, payload: }
   end
 end
--- spec/services/shard_action_broadcaster_service_spec.rb
+++ spec/services/shard_action_broadcaster_service_spec.rb
@@ -3,12 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ShardActionBroadcasterService do
-  subject(:call) { described_class.call(shard, action) }
+  subject(:call) { described_class.call(shard, action, payload) }
 
   let(:shard) { build(:shard_connection_data) }
   let(:action) { 'action' }
+  let(:payload) { 42 }
 
   it do
-    expect { call }.to have_broadcasted_to('shards/0_1_42').with(action: 'action')
+    expect { call }.to have_broadcasted_to('shards/0_1_42').with(action: 'action', payload: 42)
   end
 end
